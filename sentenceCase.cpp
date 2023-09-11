789b36750f51d806c2a55ec8addda22dcadf39ae
Okms213 <139421807+Okms213@users.noreply.github.com>
2023-09-11 20:31:07 +0300

    Update sentenceCase.cpp

--- sentenceCase.cpp
+++ sentenceCase.cpp
@@ -1,48 +1,4 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-bool isAlphabet(char a){
-	return isalpha(a); //isalpha is bool function to check if a char is an alphabet.
-}
-
-bool isPuncOrNum(char a){
-	return ispunct(a)||isdigit(a); //ispunct/digit is a bool function to check if a char is a punctuation mark/digit.
-}
-
-
-string firstLetterUpperCase(string &s){
-
-	if(s.length()>1){
-	string sub=s.substr(1);
-	char first = toupper(s[0]);
-	s=first+sub;
-	return s;
-	}
-	//example: "hello"--> sub = "ello" & first='h' , answer = first + sub;
-
-
-	else{
-		char x=toupper(s[0]);
-		string letter="";
-		letter+=x;
-		return letter;
-	}
-	/*this else statement is when the desired string is a single character like "c",
-		 then it just converts it to "C" without additional operations.*/
-}
-
-string getSubstring(int begin,int end,string x){
-	string sub="";
-	for(int i=begin;i<end;i++){
-		sub+=x[i];
-	}
-	return sub;
-
-	/*the reason I used a specific function for substring and not just use .substr is 
-	just because .substr output unexpected results,so I did one myself.*/
-}
-
+#include "sCasefunctions.h"
 
 void sentenceCase(){
 	 
@@ -58,7 +14,7 @@ pair<int,char> temporary;
 
 for(int i=0;i<Words.length();i++){
 
-	if(isPuncOrNum(Words[i])){
+	if(isPuncOrDigit(Words[i])){
 
 		temporary.first=i;
 
@@ -76,14 +32,14 @@ vector<string> seperateWords;
  //a vector that stores each word in the string seperately.
 
 int temp=0; 
-/* a variable to store the index of the last encountered letter 
+/* a var to store the index of the last encountered letter 
 so we can continue iterating and storing more words inside our vector.*/
 
 
 for(int i=0;i<Words.length();i++){
 	
 		
-	if(isAlphabet(Words[i])==false){
+	if(isAlphabet(Words[i])==0){
 		
 		
 	seperateWords.push_back(getSubstring(temp,i,Words));
@@ -93,28 +49,35 @@ for(int i=0;i<Words.length();i++){
 	}
   }
 
+  
 seperateWords.push_back(getSubstring(temp,Words.length(),Words)); 
 /*this is to add the last word to our vector because the 
 iteration goes over it.*/
   
-  
   string convertedString="";
   
   for(auto word:seperateWords){
+
   	firstLetterUpperCase(word);
   	convertedString+=(word+' ');
+
   }
   
-
 for(auto element:punctuationIndex){
 	convertedString[element.first]=element.second;
 }
 
+
+
   cout<<convertedString<<endl;
   
-}	 
-
-int main() {
-	 
-	sentenceCase();
 }
+ 		 
+	int main() {
+	 
+	 
+		
+		sentenceCase();
+		
+		
+		}
